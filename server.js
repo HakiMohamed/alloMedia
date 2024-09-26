5463f79bea47ac9d0ab9b0d5cbfa8e53b1971ce5
Mohamed Haki <Mohamedhaki70@gmail.com>
2024-09-26 14:35:30 +0100

    Réstruction de projet, changé a skeleton structure

---
+++ server.js
@@ -0,0 +1,13 @@
+//server.js
+const app = require("./app");
+const http = require("http");
+
+const port = process.env.PORT || 3000;
+
+app.set("port", port);
+
+const server = http.createServer(app);
+
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
